adf338b2fb8b07b34f47a6564d2fae4905a1fae4
vovibssnff <373265@niuitmo.ru>
2024-04-14 02:12:45 +0300

    translator added

---
+++ models/isa.go
@@ -0,0 +1,65 @@
+package models
+
+type Opcode int
+
+const (
+	//mem
+	LD = iota
+	ST
+	//nav
+	CMP
+	JZ
+	JMP
+	HLT
+	//io
+	INR
+	IN
+	OUTR
+	OUT
+	//math
+	INC
+	DEC
+	MUL
+	DIV
+	ADD
+	SUB
+	NEG
+)
+
+var Opcodes = [...]string{"mv", "st", "ld", "inc", "dec", "mul", "div", "add", "neg", "cmp", "jz", "jmp", "hlt", "out", "in"}
+
+type Data map[string]string
+
+type Operation struct {
+	Inx int    `json:"inx"`
+	Cmd string `json:"cmd"`
+	Arg string `json:"arg"`
+	Dev string `json:"dev"`
+}
+
+type KeyValuePair struct {
+	Inx   int    `json:"mem"`
+	Key   string `json:"-"`
+	Sec   string `json:"-"`
+	Value string `json:"val"`
+}
+
+type Section struct {
+	Name string
+	Inx  int
+}
+
+type MachineCode struct {
+	Data []KeyValuePair `json:"data"`
+	Ops  []Operation    `json:"ops"`
+}
+
+type Assembly struct {
+	DataSection []KeyValuePair
+	Ops         []string
+	Sections    []Section
+}
+
+func (o Opcode) ToString() string {
+	return [...]string{}[o]
+}
--- translator/translator.go
+++ translator/translator.go
@@ -1,64 +1,124 @@
 package translator
 
 import (
-	"bufio"
 	"csa_3/models"
+	"encoding/json"
 	"github.com/sirupsen/logrus"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func readFile(i string) ([]string, []string) {
-	in, err := os.Open(i)
+func ParseAssemblyCode(filename string) (models.Assembly, error) {
+	var dataSection []models.KeyValuePair
+	ops := make([]string, 0)
+	var sections []models.Section
+
+	content, err := os.ReadFile(filename)
 	if err != nil {
-		logrus.Error(err)
-		logrus.Exit(1)
+		return models.Assembly{}, err
 	}
-	sc := bufio.NewScanner(in)
-	var data []string
-	var ops []string
-	var cur []string
-	for sc.Scan() {
-		line := strings.TrimSpace(sc.Text())
-		switch line {
-		case ".data":
-			cur = data
-		case ".ops":
-			data = cur
-			cur = ops
-		default:
-			cur = append(cur, line)
+
+	lines := strings.Split(string(content), "\n")
+	var currentSection string
+	inx := 0
+
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+		if strings.HasPrefix(line, ".") {
+			currentSection = line
+			continue
+		}
+
+		if currentSection == ".data" {
+			parts := strings.Split(line, "=")
+			if len(parts) == 2 {
+				key := strings.TrimSpace(parts[0])
+				value := strings.TrimSpace(parts[1])
+				value = strings.Trim(value, "\"")
+				dataSection = append(dataSection, models.KeyValuePair{Inx: inx, Key: key, Value: value})
+				inx += 1
+			}
+		} else if currentSection != "" {
+			sections = append(sections, models.Section{Name: currentSection, Inx: inx})
+			ops = append(ops, line)
+			inx += 1
 		}
 	}
-	if err := sc.Err(); err != nil {
-		logrus.Error(err)
-		logrus.Exit(1)
-	}
-	return data, ops
+
+	return models.Assembly{
+		DataSection: dataSection,
+		Ops:         ops,
+		Sections:    sections,
+	}, nil
 }
 
-func validOpcode(op string) bool {
-	for _, a := range models.Opcodes {
-		if a == op {
-			return true
+func TranslateAssemblyToMachine(assembly models.Assembly) (models.MachineCode, error) {
+	machine := models.MachineCode{
+		Data: assembly.DataSection,
+		Ops:  make([]models.Operation, len(assembly.Ops)),
+	}
+
+	for i, op := range assembly.Ops {
+		parts := strings.Fields(op)
+		op := parts[0]
+
+		var arg, dev string
+
+		if len(parts) > 1 {
+			for _, v := range assembly.DataSection {
+				if parts[1] == v.Key {
+					arg = strconv.Itoa(v.Inx)
+				}
+			}
+		}
+		if len(parts) > 2 {
+			for _, v := range assembly.Sections {
+				if parts[2] == v.Name {
+					arg = strconv.Itoa(v.Inx)
+					break
+				}
+			}
+
+			dev = strings.Trim(parts[1], "#")
+		}
+
+		machine.Ops[i] = models.Operation{
+			Inx: i,
+			Cmd: op,
+			Arg: arg,
+			Dev: dev,
 		}
 	}
-	return false
-}
-
-//func parseData()
-
-func toMachineCode(data []string) {
-	for _, line := range data {
-		str := strings.Split(line, " ")
-		if validOpcode(str[0]) {
-
-		}
-	}
+	return machine, nil
 }
 
 func Translate(i string, o string) {
-	logrus.Info(i, " ", o)
-	data, ops := readFile(i)
-	logrus.Info(data[1])
+	assembly, err := ParseAssemblyCode(i)
+	if err != nil {
+		logrus.Error("Error parsing .basm file: ", err)
+		return
+	}
+	logrus.Info(assembly)
+
+	machine, err := TranslateAssemblyToMachine(assembly)
+	if err != nil {
+		logrus.Error("Error translating assembly to machine code: ", err)
+		return
+	}
+
+	logrus.Info(machine)
+
+	machineJSON, err := json.MarshalIndent(machine, "", "    ")
+	if err != nil {
+		logrus.Error("Error marshalling machine code to JSON: ", err)
+		return
+	}
+	err = os.WriteFile(o, machineJSON, 0644)
+	if err != nil {
+		logrus.Error("Output file error: ", err)
+	}
 }
